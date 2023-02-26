c09988bbea263ab7fc8d5e969066c18a7f2a7c6d
Pietro <pietro.morelli@gmail.com>
2023-02-26 16:16:58 -0300

    Update script.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let valueSelector = document.querySelector("#valueSelect");
 let valueText = document.querySelector("#gridValue");
+let gridHeight = document.querySelector(".grid").offsetHeight;
+
+let boxSize = gridHeight / valueSelector.value;
 
 console.log (valueSelector);
 console.log (valueText);
+console.log (boxSize);
 
 valueSelector.oninput = function (){
     valueText.textContent = `${valueSelector.value}x${valueSelector.value}`;
@@ -14,7 +18,7 @@ valueSelector.oninput = function (){
 
 
 function createBoxes (boxQuantity){
-    for (i === boxQuantity; i > 0; i--){
-        
-    }
+    const box = document.createElement("div");
+    box.style.height = box.style.width = `${boxSize}px`;
+    box.classList.add("box");
 }
