12abda517536c1e40f66aa06a880e51162b08d24
Pietro <pietro.morelli@gmail.com>
2023-02-28 21:27:02 -0300

    Update app script.

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
 const valueSelector = document.querySelector("#valueSelect");
+const resetButton = document.querySelector("button");
 const valueText = document.querySelector("#gridValue");
-const gridHeight = document.querySelector(".grid").offsetHeight;
+const gridHeight = document.querySelector(".grid").clientHeight;
+const gridSelect = document.querySelector(".grid");
+
+let gridValue = 8;
+
 const boxes = document.querySelectorAll(".box");
 
-const boxSize = gridHeight / valueSelector.value;
+//let boxSize = (gridHeight / gridValue);
 
 console.log (valueSelector);
 console.log (valueText);
-console.log (boxSize);
+console.log (gridHeight);
+console.log (gridValue);
+//console.log (boxSize);
 
 valueSelector.oninput = function (){
+    gridValue = valueSelector.value;
     valueText.textContent = `${valueSelector.value}x${valueSelector.value}`;
+    removeBoxes(gridSelect);
+    createBoxes(gridValue);
+};
+
+
+resetButton.addEventListener('click', function(e){
+    boxes.forEach(box => {
+        box.classList.remove('active');
+    });
+});
+
+
+function createDiv (size){
+    let boxSize = (gridHeight / gridValue).toFixed(2);
+    const box = document.createElement("div");
+    box.style.height = `${boxSize}px`;
+    box.style.width = `${boxSize}px`;
+    box.classList.add("box");
+    box.addEventListener('mouseover', function (event) {
+        box.classList.add('active');
+    });
+    gridSelect.appendChild(box);
+};
+
+function createBoxes (boxQuantity){
+    for (let i = 0; i < (boxQuantity*boxQuantity) ; i++){
+        createDiv(boxQuantity);
+    }
+};
+
+function removeBoxes (parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
 };
 
 boxes.forEach( box => {
     box.addEventListener('mouseover', function (event) {
         box.classList.add('active');
-        box.style.backgroundColor = 'yellow';
-        console.log(box);
-    })
+    });
 });
-
-
-function createBoxes (boxQuantity){
-    const box = document.createElement("div");
-    box.style.height = box.style.width = `${boxSize}px`;
-    box.classList.add("box");
-};
